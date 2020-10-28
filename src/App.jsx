8dca1d70aadc0af9e78f27a31e0f0b89b8df0dd0
igmlom <62591311+igmlom@users.noreply.github.com>
2020-10-28 14:01:49 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Heading from "./components/Heading";
+import Unordred from "./components/Unordred";
+
+function App() {
+  return (
+    <div>
+      <Heading />
+      <Unordred />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Unordred.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Unorderd() {
+  return (
+    <ul>
+      <li>Banana</li>
+      <li>Groundog</li>
+      <li>Bread</li>
+    </ul>
+  );
+}
+
+export default Unorderd;
